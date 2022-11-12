a0f00c1dc59603630977186ac2eeb4a7ac194310
g020g1277_mamoru <g020g1277@g.neec.ac.jp>
2022-11-12 15:05:35 +0900

    リファクタリング

--- Blacksmith/Assets/Mamoru/SandBag.cs
+++ Blacksmith/Assets/Mamoru/SandBag.cs
@@ -4,33 +4,43 @@ using UnityEngine;
 
 public class SandBag : MonoBehaviour
 {
-    int score = 0;
-    [SerializeField] float time = 3.0f;
-    [SerializeField] const float randomRangeMin = -30.0f;
-    [SerializeField] const float randomRangeMax = 30.0f;
+    int score = 0; // スコア
+    float time; // タイムインターバル
+    const float initTime = 3.0f; // タイム初期値
+    const float randomRangeMin = -15.0f; // ランダム移動最小値
+    const float randomRangeMax = 15.0f; // ランダム移動最大値
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = initTime; // タイム初期化
     }
 
     // Update is called once per frame
     void Update()
     {
-        time -= Time.deltaTime;
+        ScoreUp(); // スコアアップ
+        RandomMove(); // ランダム移動
+    }
 
+    void ScoreUp()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             this.score++;
             Debug.Log(this.score);
         }
+    }
+
+    void RandomMove()
+    {
+        time -= Time.deltaTime;
 
         if (time <= 0)
         {
             float randomMoveX = Random.Range(randomRangeMin, randomRangeMax);
             transform.position = new Vector2(randomMoveX, 0.0f);
-            time = 3.0f;
+            time = initTime; // タイム初期化
         }
     }
 }
