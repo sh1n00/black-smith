9bfb37d17b212b8c948f484081293216849ce6c7
Kao_TzuChiao <sachinoma0408@gmail.com>
2022-11-12 14:54:45 +0900

    Tag:Sandbag作成 クリックすると色が変わるオブジェクトを作りました。

--- Blacksmith/Assets/Kou/Scripts/PlayerController.cs
+++ Blacksmith/Assets/Kou/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool isClick = false;
+    //private bool isClick = false;
 
     void Start()
     {
-        this.GetComponent<MeshRenderer>().material.color = Color.red;
+
     }
 
     
     void Update()
+    {
+        MouseLeftClick();
+        CursorObjectMovement();
+    }
+
+    //マウスクリック
+    private void MouseLeftClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RayDetect();
+        }
+    }
+
+    private void RayDetect()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            TagCheck(hit,"Sandbag");
+        }
+    }
+
+    private void TagCheck(RaycastHit hit, string tagName)
+    {
+        if(hit.transform.tag == tagName)
+        {
+            hit.collider.gameObject.GetComponent<Target>().Hit();
+        }
+    }
+
+    //カーソル
+    private void CursorObjectMovement()
     {
         //今オブジェクトのスクリーン座標
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
@@ -20,6 +54,5 @@ public class PlayerController : MonoBehaviour
         Vector3 m_MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pos.z);
         //マウス座標をワールド座標に変換してオブジェクトに渡す
         transform.position = Camera.main.ScreenToWorldPoint(m_MousePos);
-
     }
 }
