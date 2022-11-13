a3ddc1c03fb9b6377487b64ce95bd3d64f55b1e4
Kao_TzuChiao <sachinoma0408@gmail.com>
2022-11-13 11:11:52 +0900

    リザルト画面途中

---
+++ Blacksmith/Assets/Kou/Scripts/Result/EnemyScripts.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScripts : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _anim;
+
+    [SerializeField]
+    private GameObject _camera;
+    [SerializeField]
+    private Vector3 _cameraPos;
+    [SerializeField]
+    private Vector3 _thisPos;
+    private bool _isDie = false;
+
+    void Start()
+    {
+        _camera = GameObject.FindWithTag("MainCamera");
+        _thisPos = this.transform.position;
+        _anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _cameraPos = _camera.transform.position;
+        if(!_isDie)
+        {
+            if(_thisPos.x <= _cameraPos.x)
+            {
+                Debug.Log("Smash");
+                int num = Random.Range(1, 6);
+                _anim.SetInteger("SmashType",num);
+                Invoke("Destroy", 0.8f);
+                _isDie = true;
+            }
+        }
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
--- Blacksmith/Assets/shinohara/Scripts/ResultManager.cs
+++ Blacksmith/Assets/shinohara/Scripts/ResultManager.cs
@@ -2,12 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class ResultManager : MonoBehaviour
 {
+    public int Score { get; set; } = 5000;
+    private int _num;
+
+    [SerializeField]
+    private Animator _anim;
+
+    [SerializeField]
+    private GameObject _camera;
+    private bool _isCameraMove = false;
+    private bool _isCameraBack = false;
+    [SerializeField]
+    private float _dameraSpeed = 0.1f;
+    [SerializeField]
+    private float _dameraSpeedGap = 0.01f;
+
+    [SerializeField]
+    private GameObject _enemySpawnPos;
+    private Vector3 _enemySpawnPosFirst;
+    [SerializeField]
+    private GameObject _enemySandbag;
+
+    [SerializeField]
+    private Text _enemyNumText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _enemySpawnPosFirst = _enemySpawnPos.transform.position;
+        _num = Score / 100;
+        _enemyNumText.text = _num.ToString();
+        _anim = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,5 +47,58 @@ public class ResultManager : MonoBehaviour
     public void OnButtonClickTitle()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void FixedUpdate()
+    {
+        if(_isCameraMove)
+        {
+            _camera.transform.position += new Vector3(_dameraSpeed, 0, 0);
+            _dameraSpeed += _dameraSpeedGap;         
+            if(_camera.transform.position.x >= _enemySpawnPosFirst.x + 2.5f * _num)
+            {
+                TimeScaleSetting(0.1f);
+                AnimMiddleToEnd();
+                StartCoroutine(waitCorutine(0.21f));
+                _isCameraMove = false;
+            }
+        }     
+    }
+
+    public void AnimSpawnEnemy()
+    {
+        for(int i = 0; i < _num; i++)
+        {
+            _enemySpawnPos.transform.position += new Vector3(2.5f, 0, 0);
+            Instantiate(_enemySandbag, _enemySpawnPos.transform.position, Quaternion.identity);         
+        }
+    }
+
+    public void ChangeCameraMove(int num)
+    {
+        if(num == 0)
+        {
+            _isCameraMove = true;
+        }
+        else
+        {
+            _isCameraMove = false;
+        }
+    }
+
+    public void AnimMiddleToEnd()
+    {
+        _anim.SetBool("MiddleToEnd", true);
+    }
+
+    public void TimeScaleSetting(float num)
+    {
+        Time.timeScale = num;
+    }
+
+    IEnumerator waitCorutine(float waitSecond)
+    {
+        yield return new WaitForSeconds(waitSecond);
+        TimeScaleSetting(1.0f);
     }
 }
